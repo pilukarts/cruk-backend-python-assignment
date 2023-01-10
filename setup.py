30cc7100d16e680069a2fe232bcaf081f4e0f90c
Maria del Pilar Rodriguez Castillejo <pilarrodriguezcastillejo@gmail.com>
2023-01-10 03:25:18 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="example_pkg",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python ::
+setup(
+    name='mymodule',
+    version='1.0',
+    description='My Python module',
+    author='My Name',
+    packages=['mymodule'],
+    install_requires=['requests'],
+)
